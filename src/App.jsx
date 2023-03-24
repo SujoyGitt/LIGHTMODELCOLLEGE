2a4625afdd0c9bf2539493122e3797c8cfdfa800
Sujoy Ghosal <sujoy143656@gmail.com>
2023-03-24 16:47:48 +0530

    add something

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Home } from "./Home";
 import { Footer } from "./Component/Footer/Footer";
 import { Header } from "./Component/Header/Header";
 import {Route, Routes, BrowserRouter } from "react-router-dom";
+import { About } from "./Component/About/About";
+
 const App = () => {
   return (
     <>
@@ -10,6 +12,9 @@ const App = () => {
         
           <Routes>
             <Route  path="/" element={<Home/>}/>
+          </Routes>
+          <Routes>
+            <Route  path="/about" element={<About/>}/>
           </Routes>
 
     <Footer/>
---
+++ src/Component/About/About.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <h1 className='text-5xl'>Working on About us page</h1>
+  )
+}
